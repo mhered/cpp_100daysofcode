83ddcdc39fd62c80ddfe87f6150a9b87001fb391
Manuel Heredia <manolo.heredia@gmail.com>
2023-03-28 22:51:53 +0200

    Autoformatting

--- utils/init.py
+++ utils/init.py
@@ -1,32 +1,30 @@
 import os
 import datetime
 import shutil
- 
+
 parent_dir = os.getcwd()
 print(parent_dir)
-  
-date=datetime.datetime(2023,3,28)
-  
+
+date = datetime.datetime(2023, 3, 28)
+
 # Create and populate folders
-src_path=os.path.join(parent_dir, "python_code")
+src_path = os.path.join(parent_dir, "python_code")
 
 for i in range(100):
-    day= "Day"+str(i+1).zfill(3)
-    datestr=date.strftime("%d-%m-%y")
-    folder=day+"_"+datestr
-    
+    day = "Day"+str(i+1).zfill(3)
+    datestr = date.strftime("%d-%m-%y")
+    folder = day+"_"+datestr
+
     day_path = os.path.join(parent_dir, folder)
     os.mkdir(day_path)
 
-    challenge_path= os.path.join(day_path, "Challenge")
+    challenge_path = os.path.join(day_path, "Challenge")
     os.mkdir(challenge_path)
 
-    shutil.copyfile(os.path.join(src_path,"template_main.cpp"),
-                     os.path.join(challenge_path, "main.cpp"))
+    shutil.copyfile(os.path.join(src_path, "template_main.cpp"),
+                    os.path.join(challenge_path, "main.cpp"))
 
-    shutil.copyfile(os.path.join(src_path,"template_info.md"),
-                     os.path.join(challenge_path, "info.md"))
+    shutil.copyfile(os.path.join(src_path, "template_info.md"),
+                    os.path.join(challenge_path, "info.md"))
 
     date += datetime.timedelta(days=1)
-
-
--- utils/tweet.py
+++ utils/tweet.py
@@ -1,42 +1,44 @@
 import os
 import datetime
-  
-date_of_today = datetime.date.today()  
-start_date=datetime.date(2023,3,28)
 
-num_of_days = (date_of_today - start_date).days +1
+date_of_today = datetime.date.today()
+start_date = datetime.date(2023, 3, 28)
+
+num_of_days = (date_of_today - start_date).days + 1
 
 # Create folders
-day= "Day"+str(num_of_days).zfill(3)
-datestr=date_of_today.strftime("%d-%m-%y")
-folder=day+"_"+datestr
-path = os.path.join(".", folder)
+day = "Day"+str(num_of_days).zfill(3)
+datestr = date_of_today.strftime("%d-%m-%y")
+folder = day+"_"+datestr
+code_dir = os.path.join(os.getcwd(), "code")
+path = os.path.join(code_dir, folder)
 
-challenges = next(os.walk(path))[1] # get list of subfolders
+challenges = next(os.walk(path))[1]  # get list of subfolders
 
-links =[]
+links = []
 for challenge in challenges:
     info_filename = os.path.join(path, challenge, "info.md")
-    with open(info_filename) as file: # Use file to refer to the file object
+    with open(info_filename) as file:  # Use file to refer to the file object
         for line in file:
             if line.rstrip().startswith("#link:"):
                 links.append(line.split("#link:")[1].strip())
 
 
-templatetweet = "Day " + str(num_of_days) + " #100DaysOfCode #cpp progress: today "
+templatetweet = "Day " + str(num_of_days) + \
+    ": #100DaysOfCode in #Cpp progress - today "
 
 num_challenges = len(challenges)
 
 if num_challenges > 0:
-    connectors=[", "]*(num_challenges)
-    if num_challenges==1: 
-        connectors[0] = "I worked on the following challenge: "
+    connectors = [", "]*(num_challenges)
+    if num_challenges == 1:
+        connectors[0] = "I worked on the challenge "
     else:
-        connectors[0]="I worked on the following challenges: "
-        connectors[-1]=" and "
+        connectors[0] = "I worked on the challenges "
+        connectors[-1] = " and "
 
     for connector, challenge, link in zip(connectors, challenges, links):
-        tmp= connector + challenge + " " + link
+        tmp = connector + challenge + " " + link
         templatetweet += tmp
 
 else:
