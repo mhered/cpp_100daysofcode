5d165eb6b6c9554b65ebd04bd27fb7e2678880af
Manuel Heredia <manolo.heredia@gmail.com>
2023-03-29 23:31:46 +0200

    Improve init.py
    - update new name of folders `utils` and `code`
    - add offset to allow making changes to folders beyond a given day
    - modify to create two Challenge folders each day
    - add comments

--- utils/init.py
+++ utils/init.py
@@ -8,23 +8,29 @@ print(parent_dir)
 date = datetime.datetime(2023, 3, 28)
 
 # Create and populate folders
-src_path = os.path.join(parent_dir, "python_code")
+src_path = os.path.join(parent_dir, "utils")
+code_path = os.path.join(parent_dir, "code")
 
-for i in range(100):
-    day = "Day"+str(i+1).zfill(3)
-    datestr = date.strftime("%d-%m-%y")
-    folder = day+"_"+datestr
 
-    day_path = os.path.join(parent_dir, folder)
-    os.mkdir(day_path)
+# offset >0 to modify folders beyond a given day (if offset = N first day affected is N+1)
+offset = 4
+date += datetime.timedelta(days=offset)
 
-    challenge_path = os.path.join(day_path, "Challenge")
-    os.mkdir(challenge_path)
+for i in range(offset,100):
+    day_str = "Day"+str(i+1).zfill(3)
+    date_str = date.strftime("%d-%m-%y")
 
-    shutil.copyfile(os.path.join(src_path, "template_main.cpp"),
-                    os.path.join(challenge_path, "main.cpp"))
+    day_path = os.path.join(code_path, "_".join([day_str,date_str]))
+    os.mkdir(day_path) # this fails if folder already exists
 
-    shutil.copyfile(os.path.join(src_path, "template_info.md"),
-                    os.path.join(challenge_path, "info.md"))
+    for j in range(2):
+        challenge_path = os.path.join(day_path, "Challenge"+str(j+1))
+        os.mkdir(challenge_path)
+
+        shutil.copyfile(os.path.join(src_path, "template_main.cpp"),
+                        os.path.join(challenge_path, "main.cpp"))
+
+        shutil.copyfile(os.path.join(src_path, "template_info.md"),
+                        os.path.join(challenge_path, "info.md"))
 
     date += datetime.timedelta(days=1)
