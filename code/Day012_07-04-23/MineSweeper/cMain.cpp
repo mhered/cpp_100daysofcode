d8cd79809813e7fa222f644d80715507d24378bb
Manuel Heredia <manolo.heredia@gmail.com>
2023-04-16 18:23:49 +0200

    Add colors to Minesweeper

--- code/Day012_07-04-23/MineSweeper/cMain.cpp
+++ code/Day012_07-04-23/MineSweeper/cMain.cpp
@@ -17,17 +17,17 @@ wxBEGIN_EVENT_TABLE(cMain, wxFrame)
 {
     Centre();
     // create array of buttons
-    btn = new wxButton *[n_field_width * n_field_height];
+    btn = new wxButton *[N_BOARD_WIDTH * N_BOARD_HEIGHT];
 
     // create a grid sizer to distribute the buttons
     wxGridSizer *grid = new wxGridSizer(
-        n_field_width,
-        n_field_height,
+        N_BOARD_WIDTH,
+        N_BOARD_HEIGHT,
         5, // this is padding
         5);
 
     // create the mine field
-    n_field = new int[n_field_width * n_field_height];
+    n_board = new int[N_BOARD_WIDTH * N_BOARD_HEIGHT];
 
     // create a font
     wxFont font(24,
@@ -36,28 +36,28 @@ wxBEGIN_EVENT_TABLE(cMain, wxFrame)
                 wxFONTWEIGHT_BOLD,
                 false);
 
-    for (int x = 0; x < n_field_width; x++)
+    for (int x = 0; x < N_BOARD_WIDTH; x++)
     {
-        for (int y = 0; y < n_field_height; y++)
+        for (int y = 0; y < N_BOARD_HEIGHT; y++)
         {
-            btn[y * n_field_width + x] = new wxButton(
+            btn[y * N_BOARD_WIDTH + x] = new wxButton(
                 this,
-                10000 + (y * n_field_width + x));
+                10000 + (y * N_BOARD_WIDTH + x));
             // set the font
-            btn[y * n_field_width + x]->SetFont(font);
+            btn[y * N_BOARD_WIDTH + x]->SetFont(font);
             // add each button to the grid sizer
             grid->Add(
-                btn[y * n_field_width + x],
+                btn[y * N_BOARD_WIDTH + x],
                 1,
                 wxEXPAND | wxALL);
 
             // dynamically link button clicked event to the handling function
-            btn[y * n_field_width + x]->Bind(
+            btn[y * N_BOARD_WIDTH + x]->Bind(
                 wxEVT_COMMAND_BUTTON_CLICKED,
                 &cMain::OnButtonClicked,
                 this);
             // set default value for mine field
-            n_field[y * n_field_width + x] = 0;
+            n_board[y * N_BOARD_WIDTH + x] = 0;
         }
     }
 
@@ -80,8 +80,8 @@ void cMain::OnButtonClicked(wxCommandEvent &event)
 {
     // I can identify the button that was clicked by ID
     // and get its x,y coordinates
-    int x = (event.GetId() - 10000) % n_field_width;
-    int y = (event.GetId() - 10000) / n_field_width;
+    int x = (event.GetId() - 10000) % N_BOARD_WIDTH;
+    int y = (event.GetId() - 10000) / N_BOARD_WIDTH;
 
     // update window title (xwString is equivalent to std::string)
     wxString txt = "Button [ " +
@@ -99,15 +99,15 @@ void cMain::OnButtonClicked(wxCommandEvent &event)
         while (mines)
         {
             // generate a random position
-            int rx = rand() % n_field_width;
-            int ry = rand() % n_field_height;
+            int rx = rand() % N_BOARD_WIDTH;
+            int ry = rand() % N_BOARD_HEIGHT;
             // and drop a mine there unless
             // there is already one or
             // it is the first pick
-            if (n_field[ry * n_field_width + rx] == 0 &&
+            if (n_board[ry * N_BOARD_WIDTH + rx] == 0 &&
                 !(rx == x && ry == y))
             {
-                n_field[ry * n_field_width + rx] = -1;
+                n_board[ry * N_BOARD_WIDTH + rx] = -1;
                 mines--;
             }
         }
@@ -115,22 +115,23 @@ void cMain::OnButtonClicked(wxCommandEvent &event)
     }
 
     // disable the button to prevent it being clicked again
-    btn[y * n_field_width + x]->Enable(false);
+    btn[y * N_BOARD_WIDTH + x]->Enable(false);
 
     // check if there was a mine
-    if (n_field[y * n_field_width + x] == -1)
+    if (n_board[y * N_BOARD_WIDTH + x] == -1)
     {
         wxMessageBox("Boom!! - Game Over");
 
         // Reset the game
         b_first_click = true;
-        for (int x = 0; x < n_field_width; x++)
+        for (int x = 0; x < N_BOARD_WIDTH; x++)
         {
-            for (int y = 0; y < n_field_height; y++)
+            for (int y = 0; y < N_BOARD_HEIGHT; y++)
             {
-                n_field[y * n_field_width + x] = 0;
-                btn[y * n_field_width + x]->SetLabel("");
-                btn[y * n_field_width + x]->Enable(true);
+                n_board[y * N_BOARD_WIDTH + x] = 0;
+                btn[y * N_BOARD_WIDTH + x]->SetLabel("");
+                btn[y * N_BOARD_WIDTH + x]->Enable(true);
+                btn[y * N_BOARD_WIDTH + x]->SetBackgroundColour(*wxWHITE);
             }
         }
     }
@@ -143,19 +144,37 @@ void cMain::OnButtonClicked(wxCommandEvent &event)
             for (int j = -1; j < 2; j++)
             {
                 if (x + i >= 0 &&
-                    x + i < n_field_width &&
+                    x + i < N_BOARD_WIDTH &&
                     y + j >= 0 &&
-                    y + j < n_field_height)
+                    y + j < N_BOARD_HEIGHT)
                 {
-                    if (n_field[(y + j) * n_field_width + (x + i)] == -1)
+                    if (n_board[(y + j) * N_BOARD_WIDTH + (x + i)] == -1)
                         mine_count++;
                 }
             }
         }
         // display number of mines if !=0
-        if (mine_count > 0)
+        if (mine_count >= 0)
         {
-            btn[y * n_field_width + x]->SetLabel(std::to_string(mine_count));
+            btn[y * N_BOARD_WIDTH + x]->SetLabel(std::to_string(mine_count));
+            switch (mine_count)
+            {
+            case 0:
+                btn[y * N_BOARD_WIDTH + x]->SetBackgroundColour(*wxLIGHT_GREY);
+                break;
+            case 1:
+                btn[y * N_BOARD_WIDTH + x]->SetForegroundColour(*wxLIGHT_GREY);
+                break;
+            case 2:
+                btn[y * N_BOARD_WIDTH + x]->SetForegroundColour(*wxGREEN);
+                break;
+            case 3:
+                btn[y * N_BOARD_WIDTH + x]->SetForegroundColour(*wxBLUE);
+                break;
+            default:
+                btn[y * N_BOARD_WIDTH + x]->SetForegroundColour(*wxRED);
+                break;
+            }
         }
     }
 
--- code/Day012_07-04-23/MineSweeper/cMain.h
+++ code/Day012_07-04-23/MineSweeper/cMain.h
@@ -13,12 +13,12 @@ public:
 
 public:
     // a 9 by 9 array of buttons
-    int n_field_width = 9;
-    int n_field_height = 9;
+    int N_BOARD_WIDTH = 9;
+    int N_BOARD_HEIGHT = 9;
 
     // array of pointers to buttons. This requires tear down!
     wxButton **btn;
-    int *n_field = nullptr; // stores the mine field
+    int *n_board = nullptr; // stores the mine field
     // mine field only populated after first click
     // so first play always works. 
     // Hence I need a flag for the first click
