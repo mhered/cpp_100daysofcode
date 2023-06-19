b3fbc5073216eaf8aa8efe3cd84223ff320cd0c4
Manuel Heredia <manolo.heredia@gmail.com>
2023-06-20 00:23:36 +0200

    Day054 - printout char code also in addition to char when key pressed

--- code/Day054_19-05-23/Keypress/main.cpp
+++ code/Day054_19-05-23/Keypress/main.cpp
@@ -1,17 +1,19 @@
 #include "kbhit.h"
 #include <iostream>
 
-int main(){
+int main()
+{
     int key_nr;
     char key;
     keyboard keyboard;
-    while(true){
-        if( keyboard.kbhit() ){
-            key_nr = keyboard.getch(); //return int
-            key = key_nr; // get ascii char
-            
-            std::cout << "You pressed:  " << key << std::endl;
-            
+    while (true)
+    {
+        if (keyboard.kbhit())
+        {
+            key_nr = keyboard.getch(); // return int
+            key = key_nr;              // get ascii char
+
+            std::cout << "You pressed:  " << key << " (" << key_nr << ")" << std::endl;
         }
     }
     return 0;
