925b422c9d4de00abf8a7482192581522ac29c20
Manuel Heredia <manolo.heredia@gmail.com>
2023-08-19 21:46:36 +0200

    Day099 - extract function amountFor()

--- code/Day099_03-07-23/Refactoring/statement.h
+++ code/Day099_03-07-23/Refactoring/statement.h
@@ -7,58 +7,76 @@
 #include <sstream>
 #include <iomanip>
 
-
-
-struct Performance {
+struct Performance
+{
     std::string playID;
     int audience;
 };
 
-struct Play {
+struct Play
+{
     std::string name;
     std::string type;
 };
 
-struct Invoice {
+struct Invoice
+{
     std::string customer;
     std::vector<Performance> performances;
 };
 
-std::string formatCurrency(double amount) {
+std::string formatCurrency(double amount)
+{
     std::ostringstream formatted;
     formatted.imbue(std::locale("en_US.UTF-8"));
     formatted << "$" << std::fixed << std::setprecision(2) << amount;
     return formatted.str();
 }
 
-std::string statement(Invoice invoice, std::map<std::string, Play> plays) {
+double amountFor(Play play, Performance perf)
+{
+    double thisAmount = 0;
+    if (play.type == "tragedy")
+    {
+        thisAmount = 40000;
+        if (perf.audience > 30)
+        {
+            thisAmount += 1000 * (perf.audience - 30);
+        }
+    }
+    else if (play.type == "comedy")
+    {
+        thisAmount = 30000;
+        if (perf.audience > 20)
+        {
+            thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+    }
+    else
+    {
+        throw std::runtime_error("unknown type: " + play.type);
+    }
+    return thisAmount;
+}
+
+std::string statement(Invoice invoice, std::map<std::string, Play> plays)
+{
     double totalAmount = 0;
     int volumeCredits = 0;
     std::string result = "Statement for " + invoice.customer + "\n";
-    
-    for (const Performance& perf : invoice.performances) {
-        const Play& play = plays[perf.playID];
-        double thisAmount = 0;
 
-        if (play.type == "tragedy") {
-            thisAmount = 40000;
-            if (perf.audience > 30) {
-                thisAmount += 1000 * (perf.audience - 30);
-            }
-        } else if (play.type == "comedy") {
-            thisAmount = 30000;
-            if (perf.audience > 20) {
-                thisAmount += 10000 + 500 * (perf.audience - 20);
-            }
-            thisAmount += 300 * perf.audience;
-        } else {
-            throw std::runtime_error("unknown type: " + play.type);
-        }
+    for (const Performance &perf : invoice.performances)
+    {
+        const Play &play = plays[perf.playID];
+
+        double thisAmount = amountFor(play, perf);
 
         // Add volume credits
         volumeCredits += std::max(perf.audience - 30, 0);
         // Add extra credit for every ten comedy attendees
-        if (play.type == "comedy") volumeCredits += std::floor(perf.audience / 5.0);
+        if (play.type == "comedy")
+            volumeCredits += std::floor(perf.audience / 5.0);
 
         // Print line for this order
         result += "  " + play.name + ": " + formatCurrency(thisAmount / 100.0) +
