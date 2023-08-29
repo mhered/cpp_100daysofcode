dcd474305ac2637cb81724b14e5ee45638ba44da
Manuel Heredia <manolo.heredia@gmail.com>
2023-08-30 00:37:04 +0200

    Day099: move createStatementData() and auxiliary functions to createStatementData.h

--- code/Day099_03-07-23/Refactoring/statement.h
+++ code/Day099_03-07-23/Refactoring/statement.h
@@ -1,44 +1,9 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-#include <cmath>
 #include <locale>
 #include <sstream>
 #include <iomanip>
-#include <numeric>
 
-struct Performance
-{
-    std::string playID;
-    int audience;
-};
-
-struct Play
-{
-    std::string name;
-    std::string type;
-};
-
-struct Invoice
-{
-    std::string customer;
-    std::vector<Performance> performances;
-};
-
-struct EnrichedPerformance
-{
-    Performance performance;
-    Play play;
-    int amount;
-    int volumeCredits;
-};
-
-struct Statement
-{
-    std::string customer;
-    std::vector<EnrichedPerformance> enrichedperformances;
-};
+#include "createStatementData.h"
 
 std::string usd(int numberCents)
 {
@@ -46,68 +11,6 @@ std::string usd(int numberCents)
     formatted.imbue(std::locale("en_US.UTF-8"));
     formatted << "$" << std::fixed << std::setprecision(2) << numberCents / 100.0;
     return formatted.str();
-}
-
-Play &playFor(Performance aPerformance, std::map<std::string, Play> plays)
-{
-    return plays[aPerformance.playID];
-}
-
-int amountFor(EnrichedPerformance aPerformance)
-{
-    int result = 0;
-
-    if (aPerformance.play.type == "tragedy")
-    {
-        result = 40000;
-        if (aPerformance.performance.audience > 30)
-        {
-            result += 1000 * (aPerformance.performance.audience - 30);
-        }
-    }
-    else if (aPerformance.play.type == "comedy")
-    {
-        result = 30000;
-        if (aPerformance.performance.audience > 20)
-        {
-            result += 10000 + 500 * (aPerformance.performance.audience - 20);
-        }
-        result += 300 * aPerformance.performance.audience;
-    }
-    else
-    {
-        throw std::runtime_error("unknown type: " + aPerformance.play.type);
-    }
-    return result;
-}
-
-int volumeCreditFor(EnrichedPerformance aPerformance)
-{
-    int result = std::max(aPerformance.performance.audience - 30, 0);
-
-    if (aPerformance.play.type == "comedy")
-        result += std::floor(aPerformance.performance.audience / 5.0);
-    return result;
-}
-
-int totalVolumeCredits(Statement data)
-{
-    return std::accumulate(data.enrichedperformances.begin(),
-                           data.enrichedperformances.end(), 0,
-                           [](int total, const EnrichedPerformance &perf)
-                           {
-                               return total + volumeCreditFor(perf);
-                           });
-}
-
-int totalAmount(Statement data)
-{
-    return std::accumulate(data.enrichedperformances.begin(),
-                           data.enrichedperformances.end(), 0,
-                           [](int total, const EnrichedPerformance &perf)
-                           {
-                               return total + amountFor(perf);
-                           });
 }
 
 std::string renderPlainText(Statement data)
@@ -121,33 +24,16 @@ std::string renderPlainText(Statement data)
                   " (" + std::to_string(perf.performance.audience) + " seats)\n";
     }
 
-    result += "Amount owed is " + usd(totalAmount(data)) + "\n";
+    result += "Amount owed is " + usd(data.totalAmount) + "\n";
     result += "You earned " +
-              std::to_string(totalVolumeCredits(data)) +
+              std::to_string(data.totalVolumeCredits) +
               " credits\n";
     return result;
 }
 
-EnrichedPerformance enrichPerformance(Performance aPerformance,
-                                      std::map<std::string, Play> plays)
-{
-    EnrichedPerformance result;
-    result.performance = aPerformance;
-    result.play = playFor(aPerformance, plays);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditFor(result);
-    return result;
-}
+
 
 std::string statement(Invoice invoice, std::map<std::string, Play> plays)
 {
-    Statement statementData;
-    statementData.customer = invoice.customer;
-
-    for (const Performance &perf : invoice.performances)
-    {
-        statementData.enrichedperformances.push_back(enrichPerformance(perf, plays));
-    }
-
-    return renderPlainText(statementData);
+    return renderPlainText(createStatementData(invoice, plays));
 }
